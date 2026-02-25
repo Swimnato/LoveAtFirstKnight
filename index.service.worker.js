3a31322f804a21297c41c9bec437124234dda6f0
Swimnato <66497456+Swimnato@users.noreply.github.com>
2026-02-25 16:08:56 -0700

    Fixed Web Export

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771963957|3066405789';
+const CACHE_VERSION = '1772059219|22585510';
 /** @type {string} */
 const CACHE_PREFIX = 'Love at First Kn-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
